340ecb33f81c3532d4589dee95425cc3838e2d33
sehyun <asdfghjkls@naver.com>
2023-04-10 18:56:30 +0900

    #2 회원가입

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,21 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public Long join(Member member){
+        Optional<Member> result =memberRepository.findByName(member.getName());
+        result.ifPresent(m->{
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+}
